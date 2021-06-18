7f787211528f390e0ba56754852fe9fd2cb48a43
Paul Hsueh <hsueh.paul@gmail.com>
2021-06-18 14:10:13 -0400

    commit tree 36402bbaf5665d8f72baaa904aba3e0eafa92afa

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+Show.create(
+    name: "Friends",
+    network: "HBO",
+    day: "June 18",
+    rating: 5,
+    season: "First",
+)
+
+Show.create(
+    name: "Game of Thrones",
+    network: "HBO",
+    day: "April 12",
+    rating: 4,
+    season: "Second"
+)
+
+Show.create(
+    name: "The Office",
+    network: "NBC",
+    day: "October 3",
+    rating: 2,
+    season: "Third"
+)
